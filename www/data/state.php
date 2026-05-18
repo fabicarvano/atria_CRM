a3f9b277137e84e50c8f71b49cd715cb616b07ff
Fabio Carvano <fabio.carvano@gmail.com>
2026-05-18 13:15:03 +0000

    Atualiza projeto e schema do banco - 2026-05-18 13:15:02

--- www/data/state.php
+++ www/data/state.php
@@ -4,7 +4,7 @@ return [
   'currencyRates' => [
     'BRL' => 1.0
   ],
-  'cacheTimestamp' => 1779109051,
-  'microtimeState' => 1779109051.775121,
+  'cacheTimestamp' => 1779110086,
+  'microtimeState' => 1779110086.304291,
   'appTimestamp' => 1778891098
 ];
